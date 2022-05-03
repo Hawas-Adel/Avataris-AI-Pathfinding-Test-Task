6b3b46e6b396236fde9ca59b9fd9449edaa78d17
Hawas Adel <36897875+Hawas-Adel@users.noreply.github.com>
2022-05-03 22:22:30 +0200

    refactoring

--- Assets/My Solution/Scripts/Utilities.cs
+++ Assets/My Solution/Scripts/Utilities.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public static class Utilities
 {
+	private const float CurvatureRadiusFactor = 2.23693629f; // CarController.CurrentSpeed multiplies speed by this value giving the wrong radius, so I'm dividing by it to get rid of it
+
 	public static Pose ToPose(this Transform transform) => new(transform.position, transform.rotation);
 
 	public static void ForEach<T>(this IEnumerable<T> collection, System.Action<T, int> action)
@@ -14,6 +16,6 @@ public static class Utilities
 		}
 	}
 
-	public static float GetRadiusOfCurvature(float speed, float turnRate) => speed / (Mathf.Deg2Rad * turnRate * 2.23693629f);
+	public static float GetRadiusOfCurvature(float speed, float turnRate) => speed / (Mathf.Deg2Rad * turnRate * CurvatureRadiusFactor);
 	public static Vector3 GetCenterOfCurvature(this Transform transform, float Radius, float steeringAngle) => transform.TransformPoint(Mathf.Sign(steeringAngle) * Radius * Vector3.right);
 }
